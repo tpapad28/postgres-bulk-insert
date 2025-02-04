3d18c07145fa5edfa08daa5c1365fe8e30e5120a
Tasos Papadopoulos <tpapad@pm.me>
2025-02-04 22:16:04 +0200

    Externalized statistics file path

--- src/main/java/com/tpapad/pbi/service/StatisticsService.java
+++ src/main/java/com/tpapad/pbi/service/StatisticsService.java
@@ -2,17 +2,23 @@ package com.tpapad.pbi.service;
 
 import com.opencsv.CSVWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 @Service
 @Slf4j
 public class StatisticsService {
+
+    @Value("${pbi.statistics.filepath}")
+    String statisticsFilePath;
+
     public void exportAndLog(String process, long duration, int rows) {
         float speed = (float) rows / duration;
-        try (CSVWriter writer = new CSVWriter(new FileWriter("src/main/resources/statistics.csv", true))) {
+        try (CSVWriter writer = new CSVWriter(new BufferedWriter(new FileWriter(statisticsFilePath, true)))) {
             writer.writeNext(new String[]{
                     process, Integer.toString(rows), Long.toString(duration), Float.toString(speed)
 
